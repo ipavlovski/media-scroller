d6138e07f77467940134696781fa20a1e11ac777
IP <ilya.pavlovski@gmail.com>
2024-01-02 13:22:57 -0400

    Setup drizzle studio.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,4 +2,8 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './schema.ts',
   out: './drizzle',
+  driver: 'better-sqlite',
+  dbCredentials: {
+    url: "./sqlite.db",
+  },
 } satisfies Config
