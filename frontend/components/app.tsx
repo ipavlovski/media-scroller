39252faad36bca7380e6f5af1e489fa97ba5d2d8
IP <ilya.pavlovski@gmail.com>
2024-01-03 01:43:13 -0400

    Add panda-css to the mix.

--- frontend/components/app.tsx
+++ frontend/components/app.tsx
@@ -1,4 +1,21 @@
+import { css } from '../styled-system/css'
+import { Center, Flex } from '../styled-system/jsx'
+
 export default function App() {
-  
-  return <h1>hello world</h1>
+  const styles = css({
+    backgroundColor: 'slate.900',
+    color: 'slate.100',
+    '& h1': {
+      _hover: {
+        background: 'blue',
+        color: 'red'
+      }
+    }
+  })
+
+  return (
+    <Flex width='100vw' height='100vh' flexDir='column' className={styles} alignItems='center'>
+      <h1>Hello test.</h1>
+    </Flex>
+  )
 }
