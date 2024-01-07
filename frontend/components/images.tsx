e31308fbcf1bad4c09edbc8efb482b54f40a0af4
IP <ilya.pavlovski@gmail.com>
2024-01-07 12:52:27 -0400

    Add auto-refresh logic to footer.

--- frontend/components/images.tsx
+++ frontend/components/images.tsx
@@ -1,4 +1,5 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 import { trpc } from '../apis/trpc'
 import { css } from '../styled-system/css'
 
@@ -8,7 +9,7 @@ const styles = {
   }),
   header: css({
     fontSize: '2rem',
-    marginTop: '3rem'
+    marginTop: '3rem',
   }),
   grid: css({
     display: 'grid',
@@ -19,6 +20,16 @@ const styles = {
   image: css({
     margin: '4px',
   }),
+  footer: css({
+    display: 'flex',
+    flexDir: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '2rem',
+    fontSize: '3rem',
+    textTransform: 'uppercase',
+    borderTop: 'solid white',
+  }),
 }
 
 const fromServer = (dir: string, img: string) => {
@@ -28,28 +39,41 @@ const fromServer = (dir: string, img: string) => {
 export default function Images() {
   console.log('Loading images...')
 
+  const { ref, inView } = useInView()
+
   // data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage
-  const { data, isSuccess, hasNextPage } = trpc.infinitePosts.useInfiniteQuery({}, {
-    getNextPageParam: (lastPage) => lastPage.nextCursor,
-    initialCursor: new Date().toISOString().substring(0, 10),
-  })
+  const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } = trpc.infinitePosts
+    .useInfiniteQuery({}, {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      initialCursor: new Date().toISOString().substring(0, 10),
+    })
+
+  useEffect(() => {
+    if (inView && hasNextPage) fetchNextPage()
+  }, [inView, hasNextPage])
 
   if (isSuccess) console.log(`success!: pages= ${data.pages.length}`)
 
   return (
-    <div className={styles.container}>
-      {data?.pages.map(({ items }) =>
-        items.map(({ date, images }) => (
-          <Fragment key={date}>
-            <h1 className={styles.header}>{date}</h1>
-            <div className={styles.grid}>
-              {images.map(({ directory, filename }) => (
-                <img className={styles.image} src={fromServer(directory, filename)} />
-              ))}
-            </div>
-          </Fragment>
-        ))
-      )}
-    </div>
+    <>
+      <div className={styles.container}>
+        {data?.pages.map(({ items }) =>
+          items.map(({ date, images }) => (
+            <Fragment key={date}>
+              <h1 className={styles.header}>{date}</h1>
+              <div className={styles.grid}>
+                {images.map(({ directory, filename }) => (
+                  <img className={styles.image} src={fromServer(directory, filename)} />
+                ))}
+              </div>
+            </Fragment>
+          ))
+        )}
+      </div>
+      <div className={styles.footer} ref={ref}>
+        <h3>This is a footer</h3>
+        {isFetchingNextPage && <p>... loading ...</p>}
+      </div>
+    </>
   )
 }
