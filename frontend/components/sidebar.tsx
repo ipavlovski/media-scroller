3dd72c74944f8c12fa5f4042e9cd133f800bcba7
IP <ilya.pavlovski@gmail.com>
2024-01-09 10:16:01 -0400

    Add search bar to UI.

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,5 +1,17 @@
 import { css } from '../styled-system/css'
 
+function SearchBar() {
+  const styles = css({
+    marginX: '1rem',
+    marginTop: '.5rem',
+    width: '10rem',
+    borderRadius: '.5rem'
+  })
+
+
+  return <input className={styles} type='search' />
+}
+
 export default function Sidebar() {
   const styles = css({
     position: 'fixed',
@@ -11,6 +23,7 @@ export default function Sidebar() {
 
   return (
     <div className={styles}>
+      <SearchBar />
       <p>test1</p>
       <p>test2</p>
     </div>
